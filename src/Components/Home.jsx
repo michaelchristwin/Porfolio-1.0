7c87bb5b31255b0b7400b7e6ecabd7ab7cb0f4f8
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-01-02 11:39:32 +0100

    Init Home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,17 @@
 import NavBar from "./JSX/NavBar";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "./CSS/styles.css";
+import Avatar from "./imgs/avatar2.jpg";
 
 function Home() {
+  AOS.init();
   return (
     <main>
       <NavBar />
+      <div data-aos="fade-up" className="shit p-3">
+        <img src={Avatar} alt="" className="dp" />
+      </div>
     </main>
   );
 }
--- src/Components/JSX/NavBar.jsx
+++ src/Components/JSX/NavBar.jsx
@@ -1,4 +1,8 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import FacebookIcon from "@mui/icons-material/Facebook";
 import "../CSS/styles.css";
 
 function NavBar() {
@@ -10,6 +14,23 @@ function NavBar() {
             <h4 className="typed">Hi, I'm Kelechukwu M. Christwin</h4>
           </Nav>
         </Container>
+        <div className="me-2 links">
+          <a href="https://twitter.com/mikechristwin" className="mx-1">
+            <TwitterIcon />
+          </a>
+          <a href="https://github.com/michaelchristwin" className="mx-1">
+            <GitHubIcon />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/michael-christwin-b8b980253/"
+            className="mx-1"
+          >
+            <LinkedInIcon />
+          </a>
+          <a href="https://www.facebook.com/kelechukwu.christwin/">
+            <FacebookIcon />
+          </a>
+        </div>
       </Navbar>
     </main>
   );
